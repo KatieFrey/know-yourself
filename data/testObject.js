4983a7eae305d4089e7bf0d6455cdc9697d89c4a
Katharine Francis <katie-francis@hotmail.com>
2020-09-23 11:44:33 -0400

    Created minature test object

---
+++ data/testObject.js
@@ -0,0 +1,66 @@
+export default [
+  {
+    1: {
+      question: "At a party do you: ",
+      a: "Interact with many, including strangers",
+      b: "Interact with a few, known to you",
+    },
+  },
+  {
+    2: {
+      question: "Are you more: ",
+      a: "realistic than speculative",
+      b: "speculative than realistic",
+    },
+  },
+  {
+    3: {
+      question: "Is it worse to: ",
+      a: "have your head in the clouds",
+      b: "be in a rut",
+    },
+  },
+  {
+    4: {
+      question: "Are you more impressed by: ",
+      a: "principles",
+      b: "emotions",
+    },
+  },
+  {
+    5: {
+      question: "Are you more drawn to: ",
+      a: "convincing",
+      b: "touching",
+    },
+  },
+  {
+    6: {
+      question: "Do you prefer to work: ",
+      a: "to deadlines",
+      b: "just whenever",
+    },
+  },
+  {
+    7: {
+      question: "Do you tend to choose: ",
+      a: "rather carefully",
+      b: "somewhat impulsively",
+    },
+  },
+  {
+    8: {
+      question: "At parties do you: ",
+      a: "stay late, with increasing energy",
+      b: "Leave early with decreased energy",
+    },
+  },
+];
+
+/*
+testObject is an array of test questions. The key being the question number and the value containing the question content
+
+The application will render each question one at a time, moving to the next question once the current question is answered.
+
+
+*/
