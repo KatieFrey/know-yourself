66a6d7a7606e1e18b152d12a6bb07928bbcae190
Katharine Francis <katie-francis@hotmail.com>
2020-09-23 13:07:30 -0400

    An attempt at adding scores to state score object

--- pages/personalitytest.js
+++ pages/personalitytest.js
@@ -1,18 +1,35 @@
 import testObject from "../data/testObject";
+import React, { useState } from "react";
 
 export default function personalitytest() {
   let counter = 0;
   let currentQuestion = testObject[counter];
+  let currentColumn = testObject[counter].personalityColumn;
+  const [score, setScore] = useState({});
+
+  const handleAnswer = (letter, column) => {
+    setScore({ ...score, [column]: letter });
+    counter++;
+  };
+
   return (
     <div>
       <h1>{`${currentQuestion.question}`}</h1>
       <div>
         {`a. ${testObject[counter].a}`}
-        <button type="input" value="a"></button>
+        <input
+          type="button"
+          value="a"
+          onClick={(e) => handleAnswer(e.target.value, currentColumn)}
+        ></input>
       </div>
       <div>
         {`b. ${testObject[counter].b}`}
-        <button type="input" value="b"></button>
+        <input
+          type="button"
+          value="b"
+          onClick={(e) => handleAnswer(e.target.value, currentColumn)}
+        ></input>
       </div>
     </div>
   );
