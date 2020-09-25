453c304209808ba5a68c854ff14adb40ade1dee0
Katharine Francis <katie-francis@hotmail.com>
2020-09-24 20:56:34 -0400

    Filled out the test object

--- pages/personalitytest.js
+++ pages/personalitytest.js
@@ -2,14 +2,32 @@ import testObject from "../data/testObject";
 import React, { useState } from "react";
 
 export default function personalitytest() {
-  let counter = 0;
+  const [score, setScore] = useState({});
+  const [counter, setCounter] = useState(0);
+
+  //In the future, I might make a result state object to collect the answers to each question "IF", for example, we want to make a graph of his answers to show if he 25% extrovert and 75% introvert. ({1:a, 2:b, ...})
+
+  console.log("Score: ", score, counter);
+
   let currentQuestion = testObject[counter];
   let currentColumn = testObject[counter].personalityColumn;
-  const [score, setScore] = useState({});
 
   const handleAnswer = (letter, column) => {
-    setScore({ ...score, [column]: letter });
-    counter++;
+    let scoreTemp = score[column] || 0;
+
+    if (letter === "a") {
+      setScore({ ...score, [column]: scoreTemp + 1 });
+    }
+
+    if (letter === "b") {
+      setScore({ ...score, [column]: scoreTemp - 1 });
+    }
+
+    //if(counter === 69) then we want to call a function that takes in the state score object and calculate the score with that object.
+    //else
+
+    let counterTemp = counter;
+    setCounter(counterTemp + 1);
   };
 
   return (
